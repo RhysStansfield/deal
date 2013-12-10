ff3a9858ce3ecdf991a16dfadd4b872252ab76c0
Michael Bottjer <michaelbottjer@hotmail.com>
2013-12-10 13:27:27 +0000

    added business categories to business sign up form and appropriate tables

--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -2,10 +2,16 @@ require 'spec_helper'
 
 describe Business do
 
+  let(:business) { FactoryGirl.create(:business) }
+
   it { should have_and_belong_to_many(:customers) }
 
   it { should have_many :offers }
 
   it { should belong_to :category}
+
+  it 'should have a category when created' do
+    expect(business.category.name).to eq 'Sports & Outdoors'
+  end
 
 end
