d26e0d7c0fffc807b85f07852ca73aa6cbd55cee
Michael Bottjer <michaelbottjer@hotmail.com>
2013-12-10 10:46:05 +0000

    Added Category model and category buttons on sign up form

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131209143739) do
+ActiveRecord::Schema.define(version: 20131210102729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "businesses", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "customers", force: true do |t|
+  create_table "categories", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -60,6 +56,7 @@ ActiveRecord::Schema.define(version: 20131209143739) do
     t.string   "provider"
     t.string   "uid"
     t.string   "name"
+    t.integer  "category_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
--- spec/models/business_spec.rb
+++ spec/models/business_spec.rb
@@ -6,4 +6,6 @@ describe Business do
 
   it { should have_many :offers }
 
+  it { should belong_to :category}
+
 end
