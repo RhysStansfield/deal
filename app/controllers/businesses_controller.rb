dc1d16d685f45432996f1ecc275646d85b9639a9
HemangPatel1 <hemang2@gmail.com>
2013-12-10 13:25:34 +0000

    unique impressions works

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -13,7 +13,14 @@ class BusinessesController < ApplicationController
   end
 
   def dashboard
-  	
+  	@business = Business.find params[:id]
+  	@offers = Offer.find(:all, conditions: { business_id: @business.id })
+  	@impressions = []
+  	@offers.each do |offer|
+  	  @impressions << Impression.find(:all, conditions: { offer_id: offer.id })
+  	end
   end
+
+
 
 end
