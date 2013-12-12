8e82d5b71a34459ba4a9f2b7f68d6305aa2130a7
Michael Bottjer <michaelbottjer@hotmail.com>
2013-12-12 14:08:42 +0000

    minor updates

--- db/schema.rb
+++ db/schema.rb
@@ -16,11 +16,6 @@ ActiveRecord::Schema.define(version: 20131211084935) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "businesses", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "categories", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
@@ -39,11 +34,6 @@ ActiveRecord::Schema.define(version: 20131211084935) do
     t.datetime "updated_at"
   end
 
-  create_table "customers", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "impressions", force: true do |t|
     t.integer  "user_id"
     t.integer  "offer_id"
@@ -52,7 +42,6 @@ ActiveRecord::Schema.define(version: 20131211084935) do
   end
 
   create_table "offers", force: true do |t|
-    t.string   "business"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "product"
@@ -61,7 +50,7 @@ ActiveRecord::Schema.define(version: 20131211084935) do
     t.integer  "business_id"
     t.datetime "available_from"
     t.datetime "available_to"
-    t.integer  "users_time_availablity"
+    t.integer  "users_time_availablity", default: 30
   end
 
   add_index "offers", ["business_id"], name: "index_offers_on_business_id", using: :btree
