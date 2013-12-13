8fe28eaceb692497f02b77fc71d1da40ea925064
jamesjenkinsyates <james@jamesjenkinsyates.com>
2013-12-13 13:18:25 +0000

    added dates to new offers

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -7,6 +7,8 @@ class Offer < ActiveRecord::Base
   has_many :conversions
 
   validates :users_time_availablity, presence: true
+  validates :available_from, presence: true
+  validates :available_to, presence: true
 
   has_attached_file :avatar, styles: {
     large: "500x500>", medium: "300x300>", thumb: "100x100>"
