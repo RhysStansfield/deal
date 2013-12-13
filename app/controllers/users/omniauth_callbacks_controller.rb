b7eaaaad38140e38072a44d3b4a6d0e3e5317804
jamesjenkinsyates <james@jamesjenkinsyates.com>
2013-12-13 12:35:16 +0000

    Guess who is back? Twitter is back

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -23,5 +23,18 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
+
+  def twitter
+    auth = env["omniauth.auth"]
+    #Rails.logger.info("auth is **************** #{auth.to_yaml}")
+    @user = User.find_for_twitter_oauth(request.env["omniauth.auth"],current_user)
+    if @user.persisted?
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success"
+      sign_in_and_redirect @user, :event => :authentication
+    else
+      session["devise.twitter_uid"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
   
