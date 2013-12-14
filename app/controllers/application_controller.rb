60e064b8b999399682c3a6320aee012aa185a017
Hemang Patel <hemang2@gmail.com>
2013-12-14 13:02:12 +0000

    fixed returning visitor paths

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
     if current_user.type == "Business"
       dashboard_business_path(resource)
     else
-      root_path
+      offers_path
     end
   end
 
