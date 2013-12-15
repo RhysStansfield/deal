89e4d8e8d2aa57eb6b9de19f2ab2ce77b356d49d
Michael Bottjer <michaelbottjer@hotmail.com>
2013-12-15 17:57:26 +0000

    Toggle button

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'jbuilder', '~> 1.2'
 
 gem 'simple_form'
 gem 'foundation-rails'
+gem 'gruff'
 
 # User Log in Gems
 gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -56,13 +56,28 @@ class BusinessesController < ApplicationController
     # @total_revenue = @conversions.length * @offers.length
   end
 
+  def line_graph_report
+    g = Gruff::Line.new(400)
+    g.title = "Scores for Bart" 
+    g.font = File.expand_path('artwork/fonts/Vera.ttf', RAILS_ROOT)
+    g.labels = { 0 => 'Mon', 2 => 'Wed', 4 => 'Fri', 6 => 'Sun' }
+
+    # Modify this to represent your actual data models
+    @data = Data.find(:all)
+    @data.each do |d|
+      # Build data into array with something like
+      # built_array = d.collect { |e| e.some_number_field.to_f }
+      g.data(some_label, built_array)
+    end
+
+    send_data(g.to_blob, 
+              :disposition => 'inline', 
+              :type => 'image/png', 
+              :filename => "bart_scores.png")
+  end
 
   def sign_up_params
     params[:business].permit(:email, :password, :password_confirmation, :company_name, :category_id)
   end
-
-  
-
-
 
 end
