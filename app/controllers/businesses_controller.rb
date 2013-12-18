2c5c65232b5cecaf2d45239a664f82a525e0f8f6
jamesjenkinsyates <james@jamesjenkinsyates.com>
2013-12-18 11:39:12 +0000

    added

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -1,4 +1,13 @@
 class BusinessesController < ApplicationController
+  # before_filter :authenticate_business!, only: [:dashboard]
+
+  # def authenticate_business!
+  #   @business = Business.find params[:id]
+
+  #   if !current_business || current_business != @business
+  #     raise 'Unauthorized'
+  #   end
+  # end
 
   def index
     if request.xhr?
@@ -17,7 +26,7 @@ class BusinessesController < ApplicationController
     if @business.save
       flash[:notice] = 'Signed up as a business'
       sign_in(:user, @business)
-      redirect_to dashboard_business_path(@business)
+      redirect_to dashboard_path
     else
       render 'new'
     end
@@ -42,7 +51,9 @@ class BusinessesController < ApplicationController
   end
 
   def dashboard
-  	@business = Business.find params[:id]
+  	@business = current_business
+    redirect_to '/' and return unless @business
+
     @all_statistics = [Impression, Conversion, Click]
   	@offers = @business.offers
   end
