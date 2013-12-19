94a94199055ff83bbf9fa717d336350e13ac1195
Nadia Odunayo <nodunayo@gmail.com>
2013-12-19 16:30:13 +0000

    Updated README

--- README.md
+++ README.md
@@ -3,8 +3,28 @@ Makers Academy Final Project: Deal
 A snapchat and groupon inspired app developed for Makers Academy final project.
 
   Team:
+
+
+
+
+
+Aim
+Other Details
+To Do
+
+Screenshots
+
+Link to Heroku
+
+
+Testing
+
+Team
       <br>Hemang Patel<br>
       Michael Bottjer<br>
       Nadia Odunayo<br>
       Rhys Stansfield<br>
       [James Jenkins-Yates](http://jamesjenkinsyates.com)<br>
+
+
+
--- spec/helpers/conversions_helper_spec.rb
+++ spec/helpers/conversions_helper_spec.rb
@@ -1,15 +1,15 @@
-require 'spec_helper'
+# require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the ConversionsHelper. For example:
-#
+# # Specs in this file have access to a helper object that includes
+# # the ConversionsHelper. For example:
+# #
+# # describe ConversionsHelper do
+# #   describe "string concat" do
+# #     it "concats two strings with spaces" do
+# #       expect(helper.concat_strings("this","that")).to eq("this that")
+# #     end
+# #   end
+# # end
 # describe ConversionsHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
+#   pending "add some examples to (or delete) #{__FILE__}"
 # end
-describe ConversionsHelper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
