0f4f8a1b76b9db485e196d603d161a07d5c396e3
Hemang Patel <hemang2@gmail.com>
2013-12-20 11:16:08 +0000

    added dynamic preview of uploaded image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -92,3 +92,17 @@ var dropdown = $('.right-dropdown').remove()
 dropdown.appendTo('.home_top_width');
 
 
+$(document).ready(function(){
+    var preview = $(".upload-preview img");
+    
+    $(".file").change(function(event){
+       var input = $(event.currentTarget);
+       var file = input[0].files[0];
+       var reader = new FileReader();
+       reader.onload = function(e){
+           image_base64 = e.target.result;
+           preview.attr("src", image_base64);
+       };
+       reader.readAsDataURL(file);
+    });
+});
